06f195d9d4697505109f1aee133c05684080b0ef
Asjad <Asjad@PHANTOM>
2020-04-25 17:02:14 -0600

    popper already included

--- JS/common.js
+++ JS/common.js
@@ -1,16 +1,19 @@
 function loadIn(file, className) {
-    var rawFile = new XMLHttpRequest();
-    rawFile.open("GET", file, false);
-    rawFile.onreadystatechange = function ()
-    {
-      if(rawFile.readyState === 4)
-      {
-        if(rawFile.status === 200 || rawFile.status == 0)
-        {
-          var allText = rawFile.responseText;
-          document.getElementsByClassName(className)[0].innerHTML = allText;
-        }
-      }
-    }
-    rawFile.send(null);
-  }
+	var rawFile = new XMLHttpRequest();
+	rawFile.open("GET", file, false);
+	rawFile.onreadystatechange = function ()
+	{
+		if(rawFile.readyState === 4)
+		{
+			if(rawFile.status === 200 || rawFile.status == 0)
+			{
+				var allText = rawFile.responseText;
+				document.getElementsByClassName(className)[0].innerHTML = allText;
+			}
+		}
+	}
+	rawFile.send(null);
+}
+function getCurrentPage() {
+	return location.pathname.split('/').slice(-1)[0].split('.html')[0];
+}
--- JS/home.js
+++ JS/home.js
@@ -1,4 +1,4 @@
 $(document).ready(function() {
     //Load the header component
-    loadIn('./components/header.html', 'row header')
+    loadIn('./components/header.html', 'row header');
 })
