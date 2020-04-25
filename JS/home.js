794de88556aa384ca9b95c39afe4cf071a021f41
Asjad <Asjad@PHANTOM>
2020-04-25 17:56:04 -0600

    testing mobile dropdown

--- JS/home.js
+++ JS/home.js
@@ -1,4 +1,9 @@
 $(document).ready(function() {
     //Load the header component
     loadIn('./components/header.html', 'row header');
+    var curPage = getCurrentPage();
+    if(curPage === 'home') {
+        curPage = curPage.charAt(0).toUpperCase() + curPage.slice(1);
+        $('#dropdownMenuButton').html(curPage);
+    }
 })
